a4dec6217fc55caed5fc819bd2784ac74cb7d9b8
Maxo <maximiliano.aldecoa93@gmail.com>
2022-06-28 16:32:43 -0300

    Base tercer version

---
+++ cursos.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Fundación San Martín de Tours</title>
+    <link rel="icon" type="image/x-icon" href="assets/SMDTIcon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <img src="assets/Logo.png" id="logo" style="width: 140px; height: 80px;"></img>
+            <!--<a class="navbar-brand ml-2" href="#page-top">San Martin de Tours</a>-->
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#slideMVV">Quienes somos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="">Colaborar</a></li>
+                    <li class="nav-item"><a class="nav-link" href="">Prensa</a></li>
+                    <li class="nav-item"><a class="nav-link" href="">Cursos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="">Noticias</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+            <div class="d-flex justify-content-center">
+                <div class="text-center">
+                    <h1 class="mx-auto my-0 text-uppercase">San Martín de Tours</h1>
+                    <h2 class="text-white-50 mx-auto mt-2 mb-5">Lorem</h2>
+                    <a class="btn btn-primary" href="#slideMVV">Comenzar</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <!-- Footer-->
+    <footer class="elfooter small text-center text-white-50">
+        <div class="container-fluid p-4 text-center">
+            <section>
+                <div class="row">
+                    <h5 class="card-title m-2">Fundacion San Martín de Tours</h5>
+                    <div class="links">
+                        <a href="https://www.facebook.com/" target="_blank"><img class="m-2" src="assets/facebook.svg" alt="Facebook" width="32" height="32"></a>
+                        <a href="https://www.instagram.com/" target="_blank"><img class="m-2" src="assets/instagram.svg" alt="Intagram" width="32" height="32"></a>
+                        <a href="https://twitter.com/" target="_blank"><img class="m-2" src="assets/twitter.svg" alt="twitter" width="32" height="32"></a>
+                        <a href="https://www.youtube.com/" target="_blank"><img class="m-2" src="assets/youtube.svg" alt="Youtube" width="32" height="32"></a>
+                    </div>
+                    <div>
+                        <a class="btn btn-outline-warning m-2" style="border:none;" href="#">Encontranos</a>
+                        <a class="btn btn-outline-warning m-2" style="border:none;" href="#">Donar</a>
+                        <a class="btn btn-outline-warning m-2" style="border:none;" href="#">Estatuto</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+
+</html>
